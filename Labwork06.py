1ce6746a9187c17aecb438233b763968d30106b3
Bishanrajkc23 <bishanjung23@gmail.com>
2021-12-01 13:38:03 +0545

    all done

---
+++ Labwork06.py
@@ -0,0 +1,5 @@
+#odd even number of the first positive 10 intregers
+list1 = [1,3,5,7,9]
+list2 = [2,4,6,8,10]
+print("odd number is {} ".format(list1))
+print("even number is {} ".format(list2))
---
+++ labwork05.py
@@ -0,0 +1,5 @@
+#area of circle
+pi = 22/7
+r = int(input("Enter radius of circle"))
+a = pi*r*r
+print("area of circle is {} ".format(a))
