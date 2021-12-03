e72612d7f4f017893c2550cee07808c772a06eb3
Bishanrajkc23 <bishanjung23@gmail.com>
2021-12-03 13:01:05 +0545

    labworkday2

---
+++ DAY 2/Labwork2.py
@@ -0,0 +1,4 @@
+mass = int(input("Enter person mass: "))
+height = int(input("Enter person height: "))
+BMI = int(mass/(height*height))
+print("The BMI of person is {} kg/height\u00b2". format(BMI)  )
