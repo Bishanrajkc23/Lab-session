b179f34f2cece94a179f9eff61b736b4489230d3
Bishanrajkc23 <bishanjung23@gmail.com>
2021-12-20 21:53:33 +0545

    day6

---
+++ DAY 6/Tuple/Tuple4.py
@@ -0,0 +1,6 @@
+#Write a Python program to unpack a tuple in several variables.
+t = (1,7,78)
+a,b,c = t
+print(a)
+print(b)
+print(c)
---
+++ DAY 6/Tuple/Tuple5.py
@@ -0,0 +1,4 @@
+#Write a Python program to add an item in a tuple.
+t = (1,7,78)
+t = t + (89,)
+print(t)
